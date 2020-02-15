ce9ae86a78c31d1bcc46e9f86a577bf6856c62dd
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-02-15 17:47:52 +0530

    setting elements to linked list

--- ReverseLinkedList/ReverseLinkedList.java
+++ ReverseLinkedList/ReverseLinkedList.java
@@ -8,11 +8,16 @@ public class ReverseLinkedList {
     }
 
     public void getLinkedList() {
+        //getting elements of singly linked list
         Node n1 = new Node(10);
         Node n2 = new Node(20);
         Node n3 = new Node(30);
         Node n4 = new Node(40);
         Node n5 = new Node(50);
-
+        //setting elements to singly linked list
+        n1.setNext(n2);
+        n2.setNext(n3);
+        n3.setNext(n4);
+        n4.setNext(n5);
     }
 }
