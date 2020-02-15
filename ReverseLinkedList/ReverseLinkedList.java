0ceabd40097c09928a4f151348c46607010613f5
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-02-15 17:44:36 +0530

    getting linked list elements

---
+++ ReverseLinkedList/ReverseLinkedList.java
@@ -0,0 +1,18 @@
+package PracticeQuestion.ReverseLinkedList;
+
+public class ReverseLinkedList {
+    Node startNode = null;
+
+    public static void main(String[] args) {
+        new ReverseLinkedList();
+    }
+
+    public void getLinkedList() {
+        Node n1 = new Node(10);
+        Node n2 = new Node(20);
+        Node n3 = new Node(30);
+        Node n4 = new Node(40);
+        Node n5 = new Node(50);
+
+    }
+}
