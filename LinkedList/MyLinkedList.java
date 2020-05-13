79236063aa9e75a0ba69ef4866bae1b5dc9234cd
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-05-13 14:10:56 +0530

    append() function to add new node at the end of linked list

--- LinkedList/MyLinkedList.java
+++ LinkedList/MyLinkedList.java
@@ -3,10 +3,32 @@ package PracticeQuestion.LinkedList;
 public class MyLinkedList {
     Node head;
 
+    public void push(int newData) {                 //adding new node at front
+        Node newNode = new Node(newData);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void append(int newData) {
+        Node newNode = new Node(newData);
+        if (head == null) {
+            head = new Node(newData);
+            return;
+        }
+        newNode.next = null;
+        Node last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = newNode;
+        return;
+    }
+
     public void printList() {              //printing linked list
         Node n = head;
+        System.out.println("linked list is --->");
         while (n != null) {
-            System.out.println("LinkedList is:--" + n.data);
+            System.out.print(n.data + " ");
             n = n.next;
         }
     }
